14e653f52c7d86f5e3b68d8bd70ee1a2c695e975
Lejla Husic <80544994+lejlahusi@users.noreply.github.com>
2022-07-18 01:50:01 +0200

    New update

--- app/src/main/java/com/example/carcontrol/MainActivity.java
+++ app/src/main/java/com/example/carcontrol/MainActivity.java
@@ -1,19 +1,35 @@
 package com.example.carcontrol;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.content.ContentValues.TAG;
 
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
 
     Button automatic,manual;
 
+    String topic,clientId;
+    IMqttToken token;
+    int flag=0,flag_m=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,22 +37,94 @@ public class MainActivity extends AppCompatActivity {
         automatic = (Button) findViewById(R.id.automatic);
         manual = (Button) findViewById(R.id.manual);
 
+        clientId = MqttClient.generateClientId();
+        topic="test/topic/carcontrol";
+        MqttAndroidClient client =
+                new MqttAndroidClient(MainActivity.this, "tcp://test.mosquitto.org:1883",
+                        clientId);
+
         automatic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                                             try {
+                                                 token = client.connect();
+                                             } catch (MqttException e) {
+                                                 e.printStackTrace();
+                                             }
+                                             token.setActionCallback(new IMqttActionListener() {
+                                                 @Override
+                                                 public void onSuccess(IMqttToken asyncActionToken) {
+                                                     // We are connected
+                                                     Log.d(TAG, "onSuccess");
+                                                     flag=1;
+                                                     Toast.makeText(MainActivity.this, "connected", Toast.LENGTH_SHORT).show();
 
-                startActivity(new Intent(MainActivity.this, Automatic.class));
-            }
-        });
 
+                                                     String payload = "A";
+                                                     byte[] encodedPayload = new byte[0];
+                                                     try {
+                                                         encodedPayload = payload.getBytes("UTF-8");
+                                                         MqttMessage message = new MqttMessage(encodedPayload);
+                                                         client.publish(topic, message);
+                                                     } catch (UnsupportedEncodingException | MqttException e) {
+                                                         e.printStackTrace();
+                                                     }
+
+                                                 }
+
+                                                 @Override
+                                                 public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                                                     Toast.makeText(MainActivity.this, "not connected", Toast.LENGTH_SHORT).show();
+                                                 flag=0;
+                                                 }
+                                             });
+                                             if(flag==1){
+                                                 startActivity(new Intent(MainActivity.this, Automatic.class));
+                                             }
+                                         }
+                                     });
         manual.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Manual.class));
+                try {
+                    token = client.connect();
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+                token.setActionCallback(new IMqttActionListener(){
+                    @Override
+                    public void onSuccess(IMqttToken asyncActionToken) {
+                        // We are connected
+                       flag_m=1;
+                        Log.d(TAG, "onSuccess");
+                        Toast.makeText(MainActivity.this, "connected", Toast.LENGTH_SHORT).show();
+                        String payload = "M";
+                        byte[] encodedPayload = new byte[0];
+                        try {
+                            encodedPayload = payload.getBytes("UTF-8");
+                            MqttMessage message = new MqttMessage(encodedPayload);
+                            client.publish(topic, message);
+                        } catch (UnsupportedEncodingException | MqttException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+
+                    @Override
+                    public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                        Toast.makeText(MainActivity.this, "not connected", Toast.LENGTH_SHORT).show();
+                    flag_m=0;
+                    }
+                });
+                if(flag_m==1){
+                    startActivity(new Intent(MainActivity.this, Manual.class));
+                }
             }
         });
 
 
+        
 
         if (Build.VERSION.SDK_INT >= 21) {
             Window window = this.getWindow();
@@ -44,7 +132,9 @@ public class MainActivity extends AppCompatActivity {
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             ((Window) window).setStatusBarColor(this.getResources().getColor(R.color.orange));
             getWindow().setNavigationBarColor(getResources().getColor(R.color.orange));
-        }
+        } }
 
-    }
+
+
 }
+
--- app/src/main/java/com/example/carcontrol/Manual.java
+++ app/src/main/java/com/example/carcontrol/Manual.java
@@ -1,11 +1,27 @@
 package com.example.carcontrol;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.UnsupportedEncodingException;
 
 public class Manual extends AppCompatActivity {
 
@@ -18,30 +34,173 @@ public class Manual extends AppCompatActivity {
         Right = (ImageButton) findViewById(R.id.Right);
         Up = (ImageButton) findViewById(R.id.Up);
         Down = (ImageButton) findViewById(R.id.Down);
+        String topic,clientId;
+        final IMqttToken[] token = new IMqttToken[1];
+       /* int flag_u=0;
+        int flag_d=0;
+        int flag_r=0;
+        final int[] flag_l = {0};*/
+
+        clientId = MqttClient.generateClientId();
+        topic="test/topic/carcontrol";
+        MqttAndroidClient client =
+                new MqttAndroidClient(Manual.this, "tcp://test.mosquitto.org:1883",
+                        clientId);
 
         Left.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                try {
+                    token[0] = client.connect();
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+                token[0].setActionCallback(new IMqttActionListener() {
+                    @Override
+                    public void onSuccess(IMqttToken asyncActionToken) {
+                        // We are connected
+                        Log.d(TAG, "onSuccess");
+
+
+                        Toast.makeText(Manual.this, "connected", Toast.LENGTH_SHORT).show();
+
+
+                        String payload = "L";
+                        byte[] encodedPayload = new byte[0];
+                        try {
+                            encodedPayload = payload.getBytes("UTF-8");
+                            MqttMessage message = new MqttMessage(encodedPayload);
+                            client.publish(topic, message);
+                        } catch (UnsupportedEncodingException | MqttException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                        Toast.makeText(Manual.this, "not connected", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                });
+
             }
         });
+
+
         Right.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                try {
+                    token[0] = client.connect();
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+                token[0].setActionCallback(new IMqttActionListener() {
+                    @Override
+                    public void onSuccess(IMqttToken asyncActionToken) {
+                        // We are connected
+                        Log.d(TAG, "onSuccess");
+                      //  flag_l[0] =1;
+                        Toast.makeText(Manual.this, "connected", Toast.LENGTH_SHORT).show();
 
+
+                        String payload = "R";
+                        byte[] encodedPayload = new byte[0];
+                        try {
+                            encodedPayload = payload.getBytes("UTF-8");
+                            MqttMessage message = new MqttMessage(encodedPayload);
+                            client.publish(topic, message);
+                        } catch (UnsupportedEncodingException | MqttException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                        Toast.makeText(Manual.this, "not connected", Toast.LENGTH_SHORT).show();
+                       // flag_l[0] =0;
+                    }
+                });
             }
         });
 
         Up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                try {
+                    token[0] = client.connect();
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+                token[0].setActionCallback(new IMqttActionListener() {
+                    @Override
+                    public void onSuccess(IMqttToken asyncActionToken) {
+                        // We are connected
+                        Log.d(TAG, "onSuccess");
+                      //  flag_l[0] =1;
+                        Toast.makeText(Manual.this, "connected", Toast.LENGTH_SHORT).show();
 
+
+                        String payload = "U";
+                        byte[] encodedPayload = new byte[0];
+                        try {
+                            encodedPayload = payload.getBytes("UTF-8");
+                            MqttMessage message = new MqttMessage(encodedPayload);
+                            client.publish(topic, message);
+                        } catch (UnsupportedEncodingException | MqttException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                        Toast.makeText(Manual.this, "not connected", Toast.LENGTH_SHORT).show();
+                        //flag_l[0] =0;
+                    }
+                });
             }
         });
 
         Down.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                try {
+                    token[0] = client.connect();
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+                token[0].setActionCallback(new IMqttActionListener() {
+                    @Override
+                    public void onSuccess(IMqttToken asyncActionToken) {
+                        // We are connected
+                        Log.d(TAG, "onSuccess");
+                        //flag_l[0] =1;
+                        Toast.makeText(Manual.this, "connected", Toast.LENGTH_SHORT).show();
 
+
+                        String payload = "D";
+                        byte[] encodedPayload = new byte[0];
+                        try {
+                            encodedPayload = payload.getBytes("UTF-8");
+                            MqttMessage message = new MqttMessage(encodedPayload);
+                            client.publish(topic, message);
+                        } catch (UnsupportedEncodingException | MqttException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                        Toast.makeText(Manual.this, "not connected", Toast.LENGTH_SHORT).show();
+                       // flag_l[0] =0;
+                    }
+                });
             }
         });
     }
